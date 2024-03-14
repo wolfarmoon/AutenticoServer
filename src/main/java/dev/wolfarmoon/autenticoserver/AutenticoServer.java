44f35eccdfa1ac173bcde225984830840d074857
Wolfarmoon <44122691+wolfarmoon@users.noreply.github.com>
2024-03-14 13:25:24 -0500

    Initial commit 	new file:   .vscode/settings.json 	new file:   pom.xml 	new file:   src/main/java/dev/wolfarmoon/autenticoserver/AutenticoServer.java 	new file:   src/main/resources/plugin.yml 	new file:   target/AutenticoServer-jar-with-dependencies.jar 	new file:   target/AutenticoServer.jar 	new file:   target/classes/dev/wolfarmoon/autenticoserver/AutenticoServer.class 	new file:   target/classes/plugin.yml 	new file:   target/maven-archiver/pom.properties 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

---
+++ src/main/java/dev/wolfarmoon/autenticoserver/AutenticoServer.java
@@ -0,0 +1,17 @@
+package dev.wolfarmoon.autenticoserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutenticoServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
